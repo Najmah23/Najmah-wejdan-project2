e12ef349c77c218a32b39acd101395e8f453fd18
Wa2999 <92243050+Wa2999@users.noreply.github.com>
2021-11-18 09:26:23 +0300

    done

---
+++ src/component/Navbar.js
@@ -0,0 +1,32 @@
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+function NavbarItem() {
+  return (
+    <Navbar bg="light" expand={false}>
+      <Container fluid>
+        <div>
+          <h1>MAKEUP</h1>
+        </div>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">MAKEUP</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Link className="navbar-brand" to="/">
+                HOME
+              </Link>
+              <Link className="navbar-brand" to="/">
+                PROFILE
+              </Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavbarItem
